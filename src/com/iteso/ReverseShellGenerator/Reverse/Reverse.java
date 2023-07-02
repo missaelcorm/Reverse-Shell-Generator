aad7505cd0df3c610959b20629d969d7314ccd98
Missael Cortes <60858064+missaelcorm@users.noreply.github.com>
2023-07-01 23:39:43 -0600

    Create Reverse.java

---
+++ src/com/iteso/ReverseShellGenerator/Reverse/Reverse.java
@@ -0,0 +1,11 @@
+package com.iteso.ReverseShellGenerator.Reverse;
+
+import com.iteso.ReverseShellGenerator.ReverseShell;
+
+public class Reverse extends ReverseShell{
+
+    public Reverse(String localIP, int localPort, LISTENER listener){
+        super(localIP, localPort, listener);
+    }
+
+}
